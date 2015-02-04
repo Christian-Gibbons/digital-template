0fc355d7a0739596691cb2e5b34fe3accf8b9ed8
Christian-Gibbons <cgibbons@gmu.edu>
2015-02-04 18:41:22 -0500

    frame-1

--- js/main.js
+++ js/main.js
@@ -135,7 +135,7 @@ window.onload = function() {
 		//  Stand still
 		player.animations.stop();
 
-		player.frame = 5;
+		player.frame = -1;
 	    }
 	    
 	    //  Allow the player to jump if they are touching the ground.
