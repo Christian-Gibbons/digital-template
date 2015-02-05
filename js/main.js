44c967150e9b635825bd6d57a5c838f27068cddb
Christian-Gibbons <cgibbons@gmu.edu>
2015-02-05 05:23:53 -0500

    basic game done

--- js/main.js
+++ js/main.js
@@ -19,7 +19,9 @@ window.onload = function() {
 
 	    game.load.image('sky', 'assets/sky.png');
 	    game.load.image('ground', 'assets/platform.png');
-	    game.load.image('star', 'assets/star.png');
+		game.load.image('hydrant', 'assets/hydrant.png');
+		game.load.image('hydrant2', 'assets/hydrant2.png');
+		game.load.spritesheet('guy', 'assets/John_Sheet.png', 37, 48); 
 	    game.load.spritesheet('dog', 'assets/dog_brown_0.png', 46, 27);
 
 	}
@@ -27,8 +29,11 @@ window.onload = function() {
 	var player;
 	var platforms;
 	var cursors;
+	var jumpButton;
 
-	var stars;
+	var catcher;
+	var hydrants;
+	var hydrants2;
 	var score = 0;
 	var scoreText;
 
@@ -39,7 +44,7 @@ window.onload = function() {
 
 	    //  A simple background for our game
 	    game.add.sprite(0, 0, 'sky');
-
+		
 	    //  The platforms group contains the ground and the 2 ledges we can jump on
 	    platforms = game.add.group();
 
@@ -62,6 +67,23 @@ window.onload = function() {
 	    ledge = platforms.create(-150, 250, 'ground');
 	    ledge.body.immovable = true;
 
+		// Add fire hydrants to pee on.
+		hydrants2 = game.add.group();
+		hydrants2.enableBody = false;
+		var hydrant2;
+
+		hydrants = game.add.group();
+		hydrants.enableBody = true;
+		var hydrant;
+
+		var arrHydrantx = [20,120,420,550,770,50,200,330,400,620,730];
+		var arrHydranty = [228,228,378,378,378,514,514,514,514,514,514];
+		for(var i =0; i<arrHydrantx.length; i++){
+			hydrant2 = hydrants2.create(arrHydrantx[i],arrHydranty[i], 'hydrant2');
+			hydrant = hydrants.create(arrHydrantx[i],arrHydranty[i], 'hydrant');
+		}
+
+
 	    // The player and its settings
 	    player = game.add.sprite(32, game.world.height - 100, 'dog');
 		player.anchor.setTo(0.5,0.5);
@@ -69,39 +91,45 @@ window.onload = function() {
 	    game.physics.arcade.enable(player);
 
 	    //  Player physics properties. Give the little guy a slight bounce.
-	    player.body.bounce.y = 0.2;
-	    player.body.gravity.y = 300;
+	    player.body.bounce.y = 0.0;
+	    player.body.gravity.y = 800;
 	    player.body.collideWorldBounds = true;
 
 	    //  Our two animations, walking and peeing.
 	    player.animations.add('walk', [0, 1, 2], 10, true);
-		player.animations.add('piss', [7, 8], 10, true);
+		player.animations.add('piss', [6, 7], 10, true);
 
-	    //  Finally some stars to collect
-	    stars = game.add.group();
+		catcher = game.add.sprite(700, 350, 'guy');
+		catcher.anchor.setTo(0.5,0.5);
+		game.physics.arcade.enable(catcher);
+		catcher.body.bounce.y = 0.0;
+		catcher.body.gravity.y = 500;
+		catcher.body.collideWorldBounds = false;
+		catcher.outOfBoundsKill = true;
+		catcher.animations.add('walk', [1,2,3], 10, true);
 
-	    //  We will enable physics for any star that is created in this group
-	    stars.enableBody = true;
-
-	    //  Here we'll create 12 of them evenly spaced apart
-	    for (var i = 0; i < 12; i++)
-	    {
-		//  Create a star inside of the 'stars' group
-		var star = stars.create(i * 70, 0, 'star');
-
-		//  Let gravity do its thing
-		star.body.gravity.y = 300;
-
-		//  This just gives each star a slightly random bounce value
-		star.body.bounce.y = 0.7 + Math.random() * 0.2;
-	    }
-
+/*
+		var catchers = game.add.group();
+		catchers.enableBody = true;
+		game.physics.arcace.enable(catchers);
+		catchers.anchor.setTo(0.5,0.5);
+		catchers.body.gravity.y = 500;
+		catchers.body.bounce.y = 0.0;
+		catchers.collideWorldBounds = false;
+		catchers.outOfBoundsKill = true;
+		catchers.animations.add('walk', [1,2,3], 10, true);
+		var arrCatchx = [400, 600];
+		var arrCatchy = [game.world.height - 100, 700];
+		for(var i=0; i<arrCatchx.length; i++){
+			catcher = catchers.create(arrCatchx[i], arrCatchy[i], 'guy');
+		}
+*/
 	    //  The score
 	    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
 	    //  Our controls.
 	    cursors = game.input.keyboard.createCursorKeys();
-//	    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+	    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
 	}
 
@@ -109,64 +137,77 @@ window.onload = function() {
 
 	    //  Collide the player and the stars with the platforms
 	    game.physics.arcade.collide(player, platforms);
-	    game.physics.arcade.collide(stars, platforms);
-
-	    //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-	    game.physics.arcade.overlap(player, stars, collectStar, null, this);
-
+		game.physics.arcade.collide(catcher, platforms);
 	    //  Reset the players velocity (movement)
 	    player.body.velocity.x = 0;
 
+
+		
+		catcher.body.velocity.x = -100;
+		catcher.scale.x = -1;
+		catcher.animations.play('walk');
+
+		game.physics.arcade.overlap(player, catcher, killPlayer, null, this);
 	    if (cursors.left.isDown)
 	    {
-		//  Move to the left
-		player.body.velocity.x = -150;
+			//  Move to the left
+			player.body.velocity.x = -150;
 
-		player.scale.x = 1;
-
-		player.animations.play('walk');
+			player.scale.x = 1;
+			if(player.body.touching.down){
+				player.animations.play('walk');
+			}
+			else{
+				player.animations.stop();
+				player.frame = 0; 
+			}
 	    }
 	    else if (cursors.right.isDown)
 	    {
-		//  Move to the right
-		player.body.velocity.x = 150;
+			//  Move to the right
+			player.body.velocity.x = 150;
 
-		player.scale.x = -1;
+			player.scale.x = -1;
+			if(player.body.touching.down){
+				player.animations.play('walk');
+			}
+			else{
+				player.animations.stop();
+				player.frame = 0; 
+			}
+	    }
+	    //  Allow the player to piss if they are touching the ground and stationary.
+	    else if (cursors.up.isDown && player.body.touching.down)
+	    {
+			player.animations.play('piss');
 
-		player.animations.play('walk');
+			game.physics.arcade.overlap(player, hydrants, peeHydrant, null, this);
 	    }
 	    else
 	    {
-		//  Stand still
-		player.animations.stop();
+			//  Stand still
+			player.animations.stop();
 
-		player.frame = 0; //figure this out
+			player.frame = 5; 
 	    }
 
-/*		// Allow player to jump if they are touching the ground.
+		// Allow player to jump if they are touching the ground.
 	    if (jumpButton.isDown && player.body.touching.down)
 	    {
-		player.body.velocity.y = -350;
+			player.body.velocity.y = -600;
 	    }	    
-*/
-	    //  Allow the player to jump if they are touching the ground.
-	    if (cursors.up.isDown && player.body.touching.down)
-	    {
-		//player.body.velocity.y = -350;
-		player.animations.play('piss');
-	    }
+
 
 	}
 
-	function collectStar (player, star) {
-	    
-	    // Removes the star from the screen
-	    star.kill();
-
-	    //  Add and update the score
-	    score += 10;
-	    scoreText.text = 'Score: ' + score;
-
+	function peeHydrant (player, hydrant) {
+		hydrant.kill();
+		score += 100;
+		scoreText.text = 'Score: ' + score;
 	}
 
+	function killPlayer (player, catcher) {
+		player.kill();
+		scoreText.text = 'Score: ' + score + '            GAME OVER';
+	}
 };
