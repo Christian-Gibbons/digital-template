9bee0da60b179e88408d232aa8806801f9dc0113
Christian-Gibbons <cgibbons@gmu.edu>
2015-02-04 19:34:25 -0500

    now with more piss

--- js/main.js
+++ js/main.js
@@ -101,7 +101,8 @@ window.onload = function() {
 
 	    //  Our controls.
 	    cursors = game.input.keyboard.createCursorKeys();
-	    
+	    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
 	}
 
 	function update() {
@@ -141,7 +142,12 @@ window.onload = function() {
 
 		player.frame = 6; //figure this out
 	    }
-	    
+
+	    if (jumpButton.isDown && player.body.touching.down)
+	    {
+		player.body.velocity.y = -350;
+	    }	    
+
 	    //  Allow the player to jump if they are touching the ground.
 	    if (cursors.up.isDown && player.body.touching.down)
 	    {
